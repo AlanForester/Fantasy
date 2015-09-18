df9301c25b14d36b43daa67cb0bad8d1ecc5cad2
Alex Collin <alex@collin.su>
2015-09-18 17:37:18 +0300

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150915192508) do
+ActiveRecord::Schema.define(version: 20150918134115) do
 
   create_table "proposals", force: :cascade do |t|
-    t.string   "title",      limit: 255
-    t.text     "text",       limit: 65535
+    t.text     "idea",       limit: 65535
+    t.integer  "creator_id", limit: 4
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "refutations", force: :cascade do |t|
+    t.text     "proof",      limit: 65535
+    t.integer  "creator_id", limit: 4
+    t.integer  "parent_id",  limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",   limit: 255
+    t.string   "email",      limit: 255
+    t.string   "password",   limit: 255
+    t.integer  "rating",     limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
