f30c6875ea0d2aac8525bfa1ea42517033452fb7
Alex Collin <alex@collin.su>
2015-09-24 23:54:28 +0300

    эээ

--- db/migrate/20150918134112_create_proposals.rb
+++ db/migrate/20150918134112_create_proposals.rb
@@ -2,7 +2,7 @@ class CreateProposals < ActiveRecord::Migration
   def change
     create_table :proposals do |t|
       t.text :idea
-      t.references :users, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
       t.timestamps null: false
     end
   end
--- db/migrate/20150918134115_create_refutations.rb
+++ db/migrate/20150918134115_create_refutations.rb
@@ -2,7 +2,7 @@ class CreateRefutations < ActiveRecord::Migration
   def change
     create_table :refutations do |t|
       t.text :proof
-      t.references :users, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
       t.references :proposal, index: true, foreign_key: true
       t.timestamps null: false
     end
